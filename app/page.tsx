e6c32b9f57ed5cd62e40764ef6c1c4cbe31fc3c6
Keller.mp4 <121843701+kellermp4@users.noreply.github.com>
2025-03-25 16:37:02 -0400

    make bg black and add hero text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <p>hello</p>
+    <>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Spotify, but better.</h1>
+    </div>
+    </>
   );
 }

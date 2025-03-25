9a287e134fcb129ab492c71d1d6ac101f14ba032
Keller.mp4 <121843701+kellermp4@users.noreply.github.com>
2025-03-25 16:40:23 -0400

    change font to geist

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen hero-text">
       <h1>Spotify, but better.</h1>
     </div>
     </>
